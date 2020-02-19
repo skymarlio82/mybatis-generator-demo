3b7bb3ddf9589b79bccaa27b3878cea4bef291cd
marlio <jitao.liu82@gmail.com>
2020-02-19 21:30:15 +0800

    db file to initialize

---
+++ src/main/resources/db/dataloading.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `t_user` (`user_id`,`user_name`,`password`,`phone`) VALUES (1,'test','password','15611111111');
+INSERT INTO `t_user` (`user_id`,`user_name`,`password`,`phone`) VALUES (2,'test1','password','15622222222');
+INSERT INTO `t_user` (`user_id`,`user_name`,`password`,`phone`) VALUES (3,'test2','password','15633333333');
---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE `test01`;
+
+CREATE TABLE `t_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
